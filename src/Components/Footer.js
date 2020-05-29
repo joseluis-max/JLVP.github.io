0d597fbce274d368963b6c87438975cb675d7d52
Erick Peña <erickscomputer@Ericks-MBP.mshome.net>
2020-05-29 03:30:11 -0400

    cleaning unused code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,22 +11,15 @@ class Footer extends Component {
 
     return (
       <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
+        <div className="row">
+            <div className="twelve columns">
+              <ul className="social-links">
+                  {networks}
+              </ul>
+            </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,26 +5,8 @@ import 'react-animated-slider/build/horizontal.css';
 class Portfolio extends Component {
   render() {
     if(this.props.data){
-      // var projects = this.props.data.projects.map(function(projects){
-      //   var projectImage = 'images/portfolio/'+projects.image;
-      //   return <div key={projects.title} className="columns portfolio-item">
-      //      <div className="item-wrap">
-      //       <a href={projects.url} title={projects.title}>
-      //          <img alt={projects.title} src={projectImage} />
-      //          <div className="overlay">
-      //             <div className="portfolio-item-meta">
-      //            <h5>{projects.title}</h5>
-      //                <p>{projects.category}</p>
-      //             </div>
-      //           </div>
-      //         <div className="link-icon"><i className="fa fa-link"></i></div>
-      //       </a>
-      //     </div>
-      //   </div>
-      // })
-
+  
       var slides = this.props.data.check.map(function(slide, index) { 
-
         var examples = slide.images.map(function(image, index) {
           return (
             <img key={index} src={'images/portfolio/' + image} alt={slide.title + " example #" + index} height="250" width="250"/> 
